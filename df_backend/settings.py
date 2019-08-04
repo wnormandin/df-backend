08c2eed492a820cd6ccc85e22cb2b7799ca229c4
pokeybill <bill@pokeybill.us>
2019-08-03 22:54:39 -0500

    Added ui app to settings.py

--- df_backend/settings.py
+++ df_backend/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'df_backend.api'
+    'df_backend.api',
+    'df_backend.ui'
 ]
 
 MIDDLEWARE = [
